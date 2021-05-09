fb6b0552c2f6262fafd26b257c8dfafeb6136d40
vanitajavade <vanitajavade@gmail.com>
2021-05-09 18:40:43 +0530

    index page done, contact page working

--- js/custom.js
+++ js/custom.js
@@ -1,68 +1,31 @@
 $(document).ready(function(){
-
-var current_fs, next_fs, previous_fs; //fieldsets
-var opacity;
-
-$(".next").click(function(){
-
-current_fs = $(this).parent();
-next_fs = $(this).parent().next();
-
-//Add Class Active
-$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
-//show the next fieldset
-next_fs.show();
-//hide the current fieldset with style
-current_fs.animate({opacity: 0}, {
-step: function(now) {
-// for making fielset appear animation
-opacity = 1 - now;
-
-current_fs.css({
-'display': 'none',
-'position': 'relative'
-});
-next_fs.css({'opacity': opacity});
-},
-duration: 600
-});
-});
-
-$(".previous").click(function(){
-
-current_fs = $(this).parent();
-previous_fs = $(this).parent().prev();
-
-//Remove class active
-$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-//show the previous fieldset
-previous_fs.show();
-
-//hide the current fieldset with style
-current_fs.animate({opacity: 0}, {
-step: function(now) {
-// for making fielset appear animation
-opacity = 1 - now;
-
-current_fs.css({
-'display': 'none',
-'position': 'relative'
-});
-previous_fs.css({'opacity': opacity});
-},
-duration: 600
-});
-});
-
-$('.radio-group .radio').click(function(){
-$(this).parent().find('.radio').removeClass('selected');
-$(this).addClass('selected');
-});
-
-$(".submit").click(function(){
-return false;
-})
-
+    $('.custom_slider .owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        items:1,
+        autoHeight:true,
+        autoplay:true, 
+        autoplayTimeout: 3000,
+        autoplaySpeed: 1000,
+    });
+    $('.customer_sec .owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        nav:false,
+        autoplay:true, 
+        autoplayTimeout: 4000,
+        autoplaySpeed: 1500,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
 });
